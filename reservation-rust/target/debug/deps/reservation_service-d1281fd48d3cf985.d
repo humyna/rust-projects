26e5c98c070a2ec8d812b5008a4167e1f75af9e6
humyna <hufo125@gmail.com>
2022-11-30 23:25:02 +0800

    setup project

---
+++ reservation-rust/target/debug/deps/reservation_service-d1281fd48d3cf985.d
@@ -0,0 +1,5 @@
+/Users/uh/Documents/sourcecode/github/rust-projects/reservation-rust/target/debug/deps/reservation_service-d1281fd48d3cf985: service/src/main.rs
+
+/Users/uh/Documents/sourcecode/github/rust-projects/reservation-rust/target/debug/deps/reservation_service-d1281fd48d3cf985.d: service/src/main.rs
+
+service/src/main.rs:
---
+++ reservation-rust/target/debug/libabi.d
@@ -0,0 +1,1 @@
+/Users/uh/Documents/sourcecode/github/rust-projects/reservation-rust/target/debug/libabi.rlib: /Users/uh/Documents/sourcecode/github/rust-projects/reservation-rust/abi/src/lib.rs
